34492f713b8b1c511f69b762b1de303edf1adf0b
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2024-02-23 18:50:46 -0300

    Registro e login de usuário

---
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+
+namespace NSE.Identidade.API.Controllers;
+
+[Route("api/identidade")]
+public class AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+    : Controller
+{
+    [HttpPost("nova-conta")]
+    public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+    {
+        if (!ModelState.IsValid) return BadRequest();
+
+        var user = new IdentityUser
+        {
+            UserName = usuarioRegistro.Email,
+            Email = usuarioRegistro.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+        if (!result.Succeeded) return BadRequest();
+        await signInManager.SignInAsync(user, false);
+        return Ok();
+
+    }
+
+    [HttpPost("autenticar")]
+    public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+    {
+        if (!ModelState.IsValid) return BadRequest();
+
+        var result = await signInManager
+            .PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
+
+        if (!result.Succeeded) return BadRequest();
+
+        return Ok();
+    }
+}
---
+++ src/services/NSE.Identidade.API/Models/UsuarioRegistro.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Identidade.API.Models;
+
+public class UsuarioRegistro
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {1} e {2} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; }
+
+    [Compare(nameof(Senha), ErrorMessage = "As senhas não conferem.")]
+    public string SenhaConfirmacao { get; set; }
+}
