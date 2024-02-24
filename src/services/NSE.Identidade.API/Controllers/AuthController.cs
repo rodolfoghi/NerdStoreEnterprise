47b47b651e844d7b0b74c889f9a339bc869ce73b
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2024-02-23 23:09:50 -0300

    Emitindo JWT pela controller

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NSE.Identidade.API.Extensions;
 using NSE.Identidade.API.Models;
 
 namespace NSE.Identidade.API.Controllers;
 
 [ApiController]
 [Route("api/identidade")]
-public class AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+public class AuthController(
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager,
+    IOptions<AppSettings> appSettings)
     : Controller
 {
     [HttpPost("nova-conta")]
@@ -25,7 +34,7 @@ public class AuthController(SignInManager<IdentityUser> signInManager, UserManag
 
         if (!result.Succeeded) return BadRequest();
         await signInManager.SignInAsync(user, false);
-        return Ok();
+        return Ok(await GerarJwt(usuarioRegistro.Email));
 
     }
 
@@ -39,6 +48,58 @@ public class AuthController(SignInManager<IdentityUser> signInManager, UserManag
 
         if (!result.Succeeded) return BadRequest();
 
-        return Ok();
+        return Ok(await GerarJwt(usuarioLogin.Email));
     }
+
+    private async Task<UsuarioRespostaLogin> GerarJwt(string email)
+    {
+        var user = await userManager.FindByEmailAsync(email);
+        var claims = await userManager.GetClaimsAsync(user);
+        var userRoles = await userManager.GetRolesAsync(user);
+
+        claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+        claims.Add(
+            new Claim(
+                JwtRegisteredClaimNames.Iat,
+                ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+        foreach (var userRole in userRoles)
+        {
+            claims.Add(new Claim("role", userRole));
+        }
+
+        var identityClaims = new ClaimsIdentity();
+        identityClaims.AddClaims(claims);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(appSettings.Value.Secret);
+        var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+        {
+            Issuer = appSettings.Value.Emissor,
+            Audience = appSettings.Value.ValidoEm,
+            Subject = identityClaims,
+            Expires = DateTime.UtcNow.AddHours(appSettings.Value.ExpiracaoHoras),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        });
+
+        var encodedToken = tokenHandler.WriteToken(token);
+
+        return new UsuarioRespostaLogin
+        {
+            AccessToken = encodedToken,
+            ExpiresIn = TimeSpan.FromHours(appSettings.Value.ExpiracaoHoras).TotalSeconds,
+            UsuarioToken = new UsuarioToken
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Claims = claims.Select(c => new UsuarioClaim { Type = c.Type, Value = c.Value })
+            }
+        };
+    }
+
+    private static long ToUnixEpochDate(DateTime date)
+        => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
 }
---
+++ src/services/NSE.Identidade.API/Models/UsuarioRespostaLogin.cs
@@ -0,0 +1,10 @@
+﻿namespace NSE.Identidade.API.Models;
+
+public class UsuarioRespostaLogin
+{
+    public string AccessToken { get; set; }
+
+    public double ExpiresIn { get; set; }
+
+    public UsuarioToken UsuarioToken { get; set; }
+}
